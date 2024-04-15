46b673934fe82510651b7a6357dc3ccdbb719b0e
Dawid <dawid.bialokryty@gmail.com>
2024-04-15 21:47:56 +0200

    added more js - for now broken - functionality for navigation, modified some sass and html kits

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,65 @@
-const burgerBtn = document.querySelector('.burger-btn')
-const navMobile = document.querySelector('.nav-mobile')
-const navMobileItems = document.querySelectorAll('.nav-mobile__link')
+const burgerBtn = document.querySelector(".burger-btn");
+const navMobile = document.querySelector(".nav-mobile");
+const navMobileItems = document.querySelectorAll(".nav-mobile__link");
+const navDesktopItems = document.querySelectorAll(".nav-desktop__link");
+const navLinkHome = document.querySelector(".nav-link-home");
+const navLinkAboutUs = document.querySelector(".nav-link-aboutus");
+const navLinkOffer = document.querySelector(".nav-link-offer");
+const navLinkContact = document.querySelector(".nav-link-contact");
+const allSections = document.querySelectorAll(".section");
 
-const handleNav = () => {
-    navMobile.classList.toggle('nav-mobile--active')
-    burgerBtn.classList.toggle('burger-btn--animation')
-}
+let naVlinkClicked = false;
 
-navMobileItems.forEach(item => {
-    item.addEventListener('click', handleNav)
-    
+const handleMobileNav = () => {
+	navMobile.classList.toggle("nav-mobile--active");
+	burgerBtn.classList.toggle("burger-btn--animation");
+};
+
+const removeActiveLink = () => {
+	navDesktopItems.forEach((item) => {
+		item.classList.remove("nav-desktop__link--active");
+	});
+};
+
+const handleDesktopNav = (e) => {
+	naVlinkClicked = true;
+	removeActiveLink();
+	e.target.classList.add("nav-desktop__link--active");
+};
+
+const handleObserver = () => {
+	if (naVlinkClicked === false) {
+		const currentSection = window.scrollY;
+		allSections.forEach((section) => {
+			if (
+				section.classList.contains("header") &&
+				section.offsetTop == currentSection
+			) {
+				removeActiveLink();
+				navLinkHome.classList.add("nav-desktop__link--active");
+			} else if (
+				section.classList.contains("about-us") &&
+				section.offsetTop <= currentSection + 90
+			) {
+				removeActiveLink();
+				navLinkAboutUs.classList.add("nav-desktop__link--active");
+			} else if (
+				section.classList.contains("offer") &&
+				section.offsetTop <= currentSection + 90
+			) {
+				removeActiveLink();
+				navLinkOffer.classList.add("nav-desktop__link--active");
+			}
+		});
+	}
+	// naVlinkClicked = false;
+};
+
+window.addEventListener("scroll", handleObserver);
+navDesktopItems.forEach((item) => {
+	item.addEventListener("click", handleDesktopNav);
 });
-
-burgerBtn.addEventListener('click', handleNav)
+navMobileItems.forEach((item) => {
+	item.addEventListener("click", handleMobileNav);
+});
+burgerBtn.addEventListener("click", handleMobileNav);
