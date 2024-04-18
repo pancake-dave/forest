252fa6ec75c6b77cca956b511f55551b1252831a
Dawid <dawid.bialokryty@gmail.com>
2024-04-18 16:24:44 +0200

    added intersection observer to js, adjusted rest of the code (now it works as intended

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,36 @@
 const burgerBtn = document.querySelector(".burger-btn");
 const navMobile = document.querySelector(".nav-mobile");
+const navDesktop = document.querySelector(".nav-desktop");
 const navMobileItems = document.querySelectorAll(".nav-mobile__link");
 const navDesktopItems = document.querySelectorAll(".nav-desktop__link");
-const navLinkHome = document.querySelector(".nav-link-home");
-const navLinkAboutUs = document.querySelector(".nav-link-aboutus");
-const navLinkOffer = document.querySelector(".nav-link-offer");
-const navLinkContact = document.querySelector(".nav-link-contact");
 const allSections = document.querySelectorAll(".section");
 
-let navLinkClicked = false;
+const navDesktopStyles = window.getComputedStyle(navDesktop);
+const navDesktopDisplay = navDesktopStyles.getPropertyValue("display");
+
+let observerEnabled = true;
 
 const handleMobileNav = () => {
 	navMobile.classList.toggle("nav-mobile--active");
 	burgerBtn.classList.toggle("burger-btn--animation");
+};
+
+const handleDesktopNav = (e) => {
+	removeActiveLink();
+	e.target.classList.add("nav-desktop__link--active");
+	observerEnabled = false;
+	window.addEventListener("scroll", () => {
+		const currentPosition = window.scrollY;
+		const target = e.target.getAttribute("href").substring(1);
+		allSections.forEach((section) => {
+			if (
+				section.classList.contains(target) &&
+				section.offsetTop == currentPosition + 90
+			) {
+				observerEnabled = true;
+			}
+		});
+	});
 };
 
 const removeActiveLink = () => {
@@ -21,60 +39,35 @@ const removeActiveLink = () => {
 	});
 };
 
-const handleDesktopNav = (e) => {
-	// navLinkClicked = true;
-	removeActiveLink();
-	e.target.classList.add("nav-desktop__link--active");
-};
-
-const handleScroll = () => {
-	if (navLinkClicked == false) {
-		const currentPosition = window.scrollY;
-		allSections.forEach((section) => {
-			if (
-				section.classList.contains("header") &&
-				section.offsetTop == currentPosition
-			) {
+const handleIntersection = (entries) => {
+	if (observerEnabled) {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				const targetId = entry.target.id;
 				removeActiveLink();
-				navLinkHome.classList.add("nav-desktop__link--active");
-			} else if (
-				section.classList.contains("about-us") &&
-				section.offsetTop <= currentPosition + 90
-			) {
-				removeActiveLink();
-				navLinkAboutUs.classList.add("nav-desktop__link--active");
-			} else if (
-				section.classList.contains("offer") &&
-				section.offsetTop <= currentPosition + 90
-			) {
-				removeActiveLink();
-				navLinkOffer.classList.add("nav-desktop__link--active");
+				navDesktopItems.forEach((item) => {
+					if (item.getAttribute("href").substring(1) === targetId) {
+						item.classList.add("nav-desktop__link--active");
+					}
+				});
 			}
 		});
 	}
 };
 
+const observer = new IntersectionObserver(handleIntersection, {
+	root: null,
+	rootMargin: "0px",
+	threshold: 1,
+});
+
+allSections.forEach((section) => {
+	observer.observe(section);
+});
 navDesktopItems.forEach((item) => {
 	item.addEventListener("click", handleDesktopNav);
-	// item.addEventListener("click", () => {
-	// 	const targetSectionId = item.getAttribute("href").substring(1);
-	// 	const targetSection = document.getElementById(targetSectionId);
-	// 	const scrollOffset = 90;
-	// 	const scrollThreshold = targetSection.offsetTop - scrollOffset;
-
-	// 	const resetNavLinkClicked = () => {
-	// 		if (window.scrollY >= scrollThreshold) {
-	// 			console.log('reset');
-	// 			navLinkClicked = false;
-	// 			window.removeEventListener("scroll", resetNavLinkClicked);
-	// 		}
-	// 	};
-
-	// 	window.addEventListener("scroll", resetNavLinkClicked);
-	// });
 });
 navMobileItems.forEach((item) => {
 	item.addEventListener("click", handleMobileNav);
 });
 burgerBtn.addEventListener("click", handleMobileNav);
-window.addEventListener("scroll", handleScroll);
