f2b4170edcd32cf496764031cf0a353dfda262a0
Dawid <dawid.bialokryty@gmail.com>
2024-04-24 20:51:59 +0200

    finished with contact page html, sass and js

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,10 +5,12 @@ const allInputs = document.querySelectorAll(".contact-input-js");
 const submitBtn = document.querySelector(".contact__form__box__btn");
 const alertMsg = document.querySelector(".contact__form__box__alert");
 const alertEmail = document.querySelector(".contact__form__box__label-warning");
+const popupElements = document.querySelectorAll(".contact-popup-js");
+const popupBtn = document.querySelector('.contact__popup__btn')
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const checkForm = () => {
-	if (nameInput.value == "" || emailInput == "" || msgInput == "") {
+	if (nameInput.value == "" || emailInput.value == "" || msgInput.value == "") {
 		alertMsg.style.display = "block";
 		if (!validateEmail(emailInput.value)) {
 			alertEmail.innerText = "- podaj poprawny adres email!";
@@ -16,11 +18,16 @@ const checkForm = () => {
 			alertEmail.innerText = "";
 		}
 	} else {
-		alertMsg.style.display = "none";
-		alertEmail.innerText = "";
-		allInputs.forEach((input) => {
-			input.value = "";
-		});
+		if (!validateEmail(emailInput.value)) {
+			alertEmail.innerText = "- podaj poprawny adres email!";
+		} else {
+			alertMsg.style.display = "none";
+			alertEmail.innerText = "";
+			allInputs.forEach((input) => {
+				input.value = "";
+			handlePupup()
+			});
+		}
 	}
 };
 
@@ -28,6 +35,12 @@ const validateEmail = (email) => {
 	return emailRegex.test(email);
 };
 
-submitBtn.addEventListener("click", checkForm);
+const handlePupup = () => {
+	popupElements.forEach((item) => {
+		item.classList.toggle('contact__popup-visible')
+	})
+}
 
-// pamiętaj żeby dodać popup
+submitBtn.addEventListener("click", checkForm);
+popupBtn.addEventListener("click", handlePupup);
+
